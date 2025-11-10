94a832af847cae68fc7e907081d297fade65359f
luly <layan552003@hotmail.com>
2025-11-10 10:54:25 +0300

    fixing server errors still

--- Backend/main.py
+++ Backend/main.py
@@ -17,7 +17,6 @@ app.add_middleware(
     allow_origins=[
         "http://127.0.0.1:5173",
         "http://localhost:5173",
-        "https://haseef.onrender.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
